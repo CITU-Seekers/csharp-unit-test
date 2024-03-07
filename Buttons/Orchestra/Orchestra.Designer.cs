2afe5ac2f938db44e097fa3baa555640534fc8dc
Mark Vincent Pausanos <work.markpausanos@gmail.com>
2024-03-07 21:20:31 +0800

    Commit

---
+++ Buttons/Orchestra/Orchestra.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace CodeChum
+{
+    partial class Orchestra
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnViolins = new Button();
+            btnCellos = new Button();
+            btnTrumpets = new Button();
+            btnDrums = new Button();
+            lblMessage = new Label();
+            SuspendLayout();
+            // 
+            // btnViolins
+            // 
+            btnViolins.Location = new Point(117, 140);
+            btnViolins.Name = "btnViolins";
+            btnViolins.Size = new Size(112, 42);
+            btnViolins.TabIndex = 0;
+            btnViolins.Text = "Violins";
+            btnViolins.UseVisualStyleBackColor = true;
+            btnViolins.Click += btnViolins_Click;
+            // 
+            // btnCellos
+            // 
+            btnCellos.Location = new Point(265, 140);
+            btnCellos.Name = "btnCellos";
+            btnCellos.Size = new Size(100, 42);
+            btnCellos.TabIndex = 1;
+            btnCellos.Text = "Cellos";
+            btnCellos.UseVisualStyleBackColor = true;
+            btnCellos.Click += btnCellos_Click;
+            // 
+            // btnTrumpets
+            // 
+            btnTrumpets.Location = new Point(411, 140);
+            btnTrumpets.Name = "btnTrumpets";
+            btnTrumpets.Size = new Size(100, 42);
+            btnTrumpets.TabIndex = 2;
+            btnTrumpets.Text = "Trumpets";
+            btnTrumpets.UseVisualStyleBackColor = true;
+            btnTrumpets.Click += btnTrumpets_Click;
+            // 
+            // btnDrums
+            // 
+            btnDrums.Location = new Point(557, 140);
+            btnDrums.Name = "btnDrums";
+            btnDrums.Size = new Size(100, 42);
+            btnDrums.TabIndex = 3;
+            btnDrums.Text = "Drums";
+            btnDrums.UseVisualStyleBackColor = true;
+            btnDrums.Click += btnDrums_Click;
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.Location = new Point(327, 246);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(38, 15);
+            lblMessage.TabIndex = 4;
+            lblMessage.Text = "label1";
+            // 
+            // Orchestra
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblMessage);
+            Controls.Add(btnDrums);
+            Controls.Add(btnTrumpets);
+            Controls.Add(btnCellos);
+            Controls.Add(btnViolins);
+            Name = "Orchestra";
+            Text = "Orchestra";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnViolins;
+        private Button btnCellos;
+        private Button btnTrumpets;
+        private Button btnDrums;
+        private Label lblMessage;
+    }
+}
---
+++ MonthCalendar/EventCountdown/EventCountdown.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CodeChum
+{
+    partial class EventCountdown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            MonthCalendar = new MonthCalendar();
+            btnAdd = new Button();
+            EventName = new TextBox();
+            lblCountdown = new Label();
+            CountdownTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // MonthCalendar
+            // 
+            MonthCalendar.Location = new Point(54, 36);
+            MonthCalendar.Name = "MonthCalendar";
+            MonthCalendar.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(206, 210);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 6;
+            btnAdd.Text = "Add Event";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // EventName
+            // 
+            EventName.Location = new Point(54, 210);
+            EventName.Multiline = true;
+            EventName.Name = "EventName";
+            EventName.Size = new Size(136, 23);
+            EventName.TabIndex = 4;
+            // 
+            // lblCountdown
+            // 
+            lblCountdown.AutoSize = true;
+            lblCountdown.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            lblCountdown.Location = new Point(54, 249);
+            lblCountdown.Name = "lblCountdown";
+            lblCountdown.Size = new Size(0, 20);
+            lblCountdown.TabIndex = 7;
+            // 
+            // CountdownTimer
+            // 
+            CountdownTimer.Tick += CountdownTimer_Tick;
+            // 
+            // EventCountdown
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(358, 354);
+            Controls.Add(lblCountdown);
+            Controls.Add(btnAdd);
+            Controls.Add(EventName);
+            Controls.Add(MonthCalendar);
+            Name = "EventCountdown";
+            Text = "EventCountdown";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MonthCalendar MonthCalendar;
+        private Button btnAdd;
+        private TextBox EventName;
+        private Label lblCountdown;
+        private System.Windows.Forms.Timer CountdownTimer;
+    }
+}
