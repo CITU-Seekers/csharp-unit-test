63a0f724e6e3195e685188234badfd24c675c7b5
Mark Vincent Pausanos <work.markpausanos@gmail.com>
2024-03-08 20:25:02 +0800

    Commit

--- CodeChumTests/obj/Debug/net6.0-windows7.0/ZZZCodeChumTests.AssemblyInfo.cs
+++ CodeChumTests/obj/Debug/net6.0-windows7.0/ZZZCodeChumTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZZZCodeChumTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2346a73c914364b8adcde129ae1e465e99f40a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZZZCodeChumTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZZZCodeChumTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
